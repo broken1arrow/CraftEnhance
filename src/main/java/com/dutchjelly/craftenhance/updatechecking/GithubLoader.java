f21da8ea31d8aa5ae2b00ef77f9d70b9905b3080
broken1arrow <broken2arrow1@gmail.com>
2022-04-10 10:51:23 +0200

    same here

--- src/main/java/com/dutchjelly/craftenhance/updatechecking/GithubLoader.java
+++ src/main/java/com/dutchjelly/craftenhance/updatechecking/GithubLoader.java
@@ -10,48 +10,48 @@ import java.net.URLConnection;
 
 public class GithubLoader {
 
-    private final String rawUrl = "https://raw.githubusercontent.com/DutchJelly/CraftEnhance/master/recent_version.txt";
+	private final String rawUrl = "https://raw.githubusercontent.com/DutchJelly/CraftEnhance/master/recent_version.txt";
 
-    private URLConnection connection;
-    private VersionChecker checker;
-    private String version;
+	private URLConnection connection;
+	private VersionChecker checker;
+	private String version;
 
-    public static GithubLoader init(VersionChecker checker){
-        GithubLoader loader = new GithubLoader();
-        loader.checker = checker;
-        if(!loader.openConnection()) return null;
+	public static GithubLoader init(VersionChecker checker) {
+		GithubLoader loader = new GithubLoader();
+		loader.checker = checker;
+		if (!loader.openConnection()) return null;
 
-        return loader;
-    }
+		return loader;
+	}
 
-    public String getVersion(){
-        return version;
-    }
+	public String getVersion() {
+		return version;
+	}
 
-    public void readVersion(){
-        InputStream inputStream;
-        ByteArrayOutputStream outputStream;
-        try {
-            outputStream = new ByteArrayOutputStream();
-            inputStream = connection.getInputStream();
-            byte[] buffer = new byte[128];
-            int i;
-            while((i = inputStream.read(buffer)) != -1)
-                outputStream.write(buffer, 0, i);
-            version = new String(outputStream.toByteArray(), "UTF-8");
-        } catch (Exception e) {
-            Messenger.Message("(fatal) The update checker could not extract the version from the url connection.");
-        }
-    }
+	public void readVersion() {
+		InputStream inputStream;
+		ByteArrayOutputStream outputStream;
+		try {
+			outputStream = new ByteArrayOutputStream();
+			inputStream = connection.getInputStream();
+			byte[] buffer = new byte[128];
+			int i;
+			while ((i = inputStream.read(buffer)) != -1)
+				outputStream.write(buffer, 0, i);
+			version = outputStream.toString("UTF-8");
+		} catch (Exception e) {
+			Messenger.Message("(fatal) The update checker could not extract the version from the url connection.");
+		}
+	}
 
-    private boolean openConnection(){
-        try {
-            connection = (new URL(rawUrl)).openConnection();
-        } catch (Exception e) {
-            Messenger.Message("(fatal) The update checker could not open URL connection.");
-            return false;
-        }
-        return true;
-    }
+	private boolean openConnection() {
+		try {
+			connection = (new URL(rawUrl)).openConnection();
+		} catch (Exception e) {
+			Messenger.Message("(fatal) The update checker could not open URL connection.");
+			return false;
+		}
+		return true;
+	}
 
 }
