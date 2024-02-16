8422c7307768c7f134a489e809102defa94efe4f
broken1arrow <16368286+broken1arrow@users.noreply.github.com>
2024-02-16 12:25:25 +0100

    Fixed logic around view recipes

--- src/main/java/com/dutchjelly/craftenhance/gui/util/FormatListContents.java
+++ src/main/java/com/dutchjelly/craftenhance/gui/util/FormatListContents.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,11 +23,11 @@ public class FormatListContents {
 		if (recipe == null) return new ArrayList<>();
 		List<ItemStack> list = new ArrayList<>(Arrays.asList(recipe.getContent()));
 		ItemStack result = recipe.getResult();
-		if (ingredientsCache != null && loadCachedItems){
+		if (ingredientsCache != null && loadCachedItems) {
 			final ItemStack itemStackResult = ingredientsCache.getItemStackResult();
 			final ItemStack[] ingredients = ingredientsCache.getItemStacks();
-			if (itemStackResult != null || ingredients != null){
-				list = new ArrayList<>(Arrays.asList(ingredients != null ? ingredients: new ItemStack[0]));
+			if (itemStackResult != null || ingredients != null) {
+				list = new ArrayList<>(Arrays.asList(ingredients != null ? ingredients : new ItemStack[0]));
 				result = itemStackResult;
 			}
 		}
@@ -43,14 +42,26 @@ public class FormatListContents {
 	}
 
 	public static List<EnhancedRecipe> canSeeRecipes(final List<EnhancedRecipe> enhancedRecipes, final Player p) {
-		return enhancedRecipes.stream().filter(x -> (
-				!self().getConfig().getBoolean("only-show-available") ||
-						x.getPermissions() == null ||
-						Objects.equals(x.getPermissions(), "") ||
-						p.hasPermission(x.getPermissions()))
-				&& (!x.isHidden() ||
-				p.hasPermission(PermissionTypes.Edit.getPerm()) ||
-				p.hasPermission(x.getPermissions() + ".hidden"))).collect(Collectors.toList());
+		return enhancedRecipes.stream().filter(
+						recipe -> canViewRecipe(recipe, p))
+				.collect(Collectors.toList());
+	}
+
+	private static boolean canViewRecipe(final EnhancedRecipe recipe, final Player p) {
+		final boolean showAvailable = !self().getConfig().getBoolean("only-show-available");
+		final boolean hasPermission = recipe.getPermissions() == null ||
+				"".equals(recipe.getPermissions()) ||
+				p.hasPermission(recipe.getPermissions());
+		final boolean isHidden = recipe.isHidden();
+		final boolean canEdit = p.hasPermission(PermissionTypes.Edit.getPerm());
+		final boolean canSeeHidden = p.hasPermission(recipe.getPermissions() + ".hidden");
+
+		if (canEdit)
+			return true;
+		if (isHidden && !canSeeHidden)
+			return false;
+
+		return showAvailable || hasPermission;
 	}
 
 	public static List<CategoryData> getCategorys(final Collection<CategoryData> categoryData, final String grupSeachFor) {
@@ -58,16 +69,18 @@ public class FormatListContents {
 			return new ArrayList<>(categoryData);
 		return categoryData.stream().filter(x -> x.getRecipeCategory().contains(grupSeachFor)).collect(Collectors.toList());
 	}
+
 	public static List<String> getCategorys(final Set<String> categoryNames, final String grupSeachFor) {
 		if (grupSeachFor == null || grupSeachFor.equals(""))
 			return new ArrayList<>(categoryNames);
 		return categoryNames.stream().filter(x -> x.contains(grupSeachFor)).collect(Collectors.toList());
 	}
+
 	public static List<Recipe> getRecipes(final List<Recipe> enabledRecipes, final List<Recipe> disabledRecipes, final boolean enableMode, final String grupSeachFor) {
 		final List<Recipe> recipes = !enableMode ? enabledRecipes : disabledRecipes;
 		if (grupSeachFor == null || grupSeachFor.equals("")) {
 			return recipes;
 		}
-		return recipes.stream().filter( recipe -> recipe.getResult().getType().name().contains(grupSeachFor.toUpperCase())).collect(Collectors.toList());
+		return recipes.stream().filter(recipe -> recipe.getResult().getType().name().contains(grupSeachFor.toUpperCase())).collect(Collectors.toList());
 	}
 }
