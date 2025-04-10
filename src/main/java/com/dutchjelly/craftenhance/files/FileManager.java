65b5f8851efd25ee54ccea2afefa6e10894d2238
Magic_Bili <124496967+addpromax@users.noreply.github.com>
2025-04-10 20:59:35 +0800

    Fix error

--- src/main/java/com/dutchjelly/craftenhance/files/FileManager.java
+++ src/main/java/com/dutchjelly/craftenhance/files/FileManager.java
@@ -1,5 +1,4 @@
 package com.dutchjelly.craftenhance.files;
-
 import com.dutchjelly.craftenhance.cache.CacheRecipes;
 import com.dutchjelly.craftenhance.CraftEnhance;
 import com.dutchjelly.craftenhance.crafthandling.recipes.EnhancedRecipe;
@@ -16,7 +15,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -30,161 +28,144 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.logging.Logger;
-
 public class FileManager {
-
-	private final boolean useJson;
-
-	private File dataFolder;
-	private File itemsFile;
-	private File recipesFile;
-	private File containerOwnerFile;
-	private File serverRecipeFile;
-	private FileConfiguration recipesConfig;
-	private FileConfiguration itemsConfig;
-	private FileConfiguration serverRecipeConfig;
-	private FileConfiguration containerOwnerConfig;
-
-	private String itemsJson;
-	private Logger logger;
-
-	private Map<String, ItemStack> items;
-	private List<EnhancedRecipe> recipes;
-	private CacheRecipes cacheRecipes;
-
-	private FileManager(final boolean useJson) {
-		this.useJson = useJson;
-	}
-
-	public static FileManager init(final CraftEnhance main) {
-//		FileManager fm = new FileManager(main.getConfig().getBoolean("use-json"));
-		final FileManager fm = new FileManager(main.getConfig().getBoolean("use-json"));
-		fm.items = new HashMap<>();
-		fm.recipes = new ArrayList<>();
-		fm.logger = main.getLogger();
-		fm.cacheRecipes =  main.getCacheRecipes();
-		fm.dataFolder = main.getDataFolder();
-		fm.dataFolder.mkdir();
-		fm.itemsFile = new File(fm.dataFolder, fm.useJson ? "items.json" : "items.yml");
-		fm.recipesFile = new File(fm.dataFolder,"recipes.yml");
-		fm.serverRecipeFile = fm.getFile("server-recipes.yml");
-		fm.containerOwnerFile = fm.getFile("container-owners.yml");
-		return fm;
-	}
-
-	@SneakyThrows
-	public static boolean EnsureResourceUpdate(final String resourceName, final File file, final FileConfiguration fileConfig, final JavaPlugin plugin) {
-		if (!file.exists()) {
-			plugin.saveResource(resourceName, false);
-			return false;
-		}
-
-		final Reader jarConfigReader = new InputStreamReader(plugin.getResource(resourceName));
-		final FileConfiguration jarResourceConfig = YamlConfiguration.loadConfiguration(jarConfigReader);
-		jarConfigReader.close();
-
-		boolean unsavedChanges = false;
-
-		for (final String key : jarResourceConfig.getKeys(false)) {
-			if (ServerVersion.newerThan(ServerVersion.V1_8))
-				if (!fileConfig.contains(key, false)) {
-					fileConfig.set(key, jarResourceConfig.get(key));
-					unsavedChanges = true;
-				} else {
-					if (!fileConfig.contains(key)) {
-						fileConfig.set(key, jarResourceConfig.get(key));
-						unsavedChanges = true;
-					}
-				}
-		}
-
-		if (unsavedChanges)
-			fileConfig.save(file);
-		return true;
-	}
-
-	private File ensureCreated(final File file) {
-		if (!file.exists()) {
-			logger.info(file.getName() + " doesn't exist... creating it.");
-			try {
-				file.createNewFile();
-			} catch (final IOException e) {
-				logger.warning("The file " + file.getName()
-						+ " couldn't be created!");
-			}
-		}
-		return file;
-	}
-
-	private File getFile(final String name) {
-		final File file = new File(dataFolder, name);
-		ensureCreated(file);
-		return file;
-	}
-
-	private FileConfiguration getYamlConfig(final File file) {
-		return YamlConfiguration.loadConfiguration(file);
-	}
-
-	public void cacheRecipes() {
-		Debug.Send("The file manager is caching recipes...");
-		EnhancedRecipe keyValue;
-		if(!recipesFile.exists())
-			return;
-
-		recipesConfig = getYamlConfig(recipesFile);
-		recipes.clear();
-		for (final String key : recipesConfig.getKeys(false)) {
-			Debug.Send("Caching recipe with key " + key);
-			keyValue = (EnhancedRecipe) recipesConfig.get(key);
-			final String validation = keyValue.validate();
-			if (validation != null) {
-				Messenger.Error("Recipe with key " + key + " has issues: " + validation);
-				Messenger.Error("This recipe will not be cached and loaded.");
-				continue;
-			}
-			keyValue.setKey(key);
-			cacheRecipes.add(keyValue);
-			//recipes.add(keyValue);
-		}
-
-		cacheRecipes.save();
-		this.recipesFile.renameTo(new File(dataFolder, "recipe_copy.yml"));
-	}
-
-	@SneakyThrows
-	public void cacheItems() {
-		if(!itemsFile.exists())
-			return;
-
-		if (useJson) {
-
-			final StringBuilder json = new StringBuilder("");
-			final Scanner scanner = new Scanner(itemsFile);
-			while (scanner.hasNextLine())
-				json.append(scanner.nextLine());
-			scanner.close();
-			items.clear();
-			final Type typeToken = new TypeToken<HashMap<String, Map<String, Object>>>() {
-			}.getType();
-			final Gson gson = new Gson();
-			final Map<String, Map<String, Object>> serialized = gson.fromJson(json.toString(), typeToken);
-			if (serialized != null)
-				serialized.keySet().forEach(x -> items.put(x, ItemStack.deserialize(serialized.get(x))));
-		} else {
-			if (itemsConfig == null)
-				itemsConfig = new YamlConfiguration();
-			itemsConfig.load(itemsFile);
-			//itemsConfig = getYamlConfig(itemsFile);
-			items.clear();
-			if (itemsConfig != null)
-				for (final String key : itemsConfig.getKeys(false)) {
-					items.put(key, itemsConfig.getItemStack(key));
-				}
-		}
-
-		this.itemsFile.renameTo(new File(dataFolder, useJson ? "items-copy.json" : "items-copy.yml"));
-	}
+    private final boolean useJson;
+    private File dataFolder;
+    private File itemsFile;
+    private File recipesFile;
+    private File containerOwnerFile;
+    private File serverRecipeFile;
+    private FileConfiguration recipesConfig;
+    private FileConfiguration itemsConfig;
+    private FileConfiguration serverRecipeConfig;
+    private FileConfiguration containerOwnerConfig;
+    private String itemsJson;
+    private Logger logger;
+    private Map<String, ItemStack> items;
+    private List<EnhancedRecipe> recipes;
+    private CacheRecipes cacheRecipes;
+    private FileManager(final boolean useJson) {
+        this.useJson = useJson;
+    }
+    public static FileManager init(final CraftEnhance main) {
+        final FileManager fm = new FileManager(main.getConfig().getBoolean("use-json"));
+        fm.items = new HashMap<>();
+        fm.recipes = new ArrayList<>();
+        fm.logger = main.getLogger();
+        fm.cacheRecipes = main.getCacheRecipes();
+        fm.dataFolder = main.getDataFolder();
+        fm.dataFolder.mkdir();
+        fm.itemsFile = new File(fm.dataFolder, fm.useJson ? "items.json" : "items.yml");
+        fm.recipesFile = new File(fm.dataFolder, "recipes.yml");
+        fm.serverRecipeFile = fm.getFile("server-recipes.yml");
+        fm.containerOwnerFile = fm.getFile("container-owners.yml");
+        
+        // load containerOwnerConfig
+        fm.containerOwnerConfig = fm.getYamlConfig(fm.containerOwnerFile);
+		fm.serverRecipeConfig = fm.getYamlConfig(fm.serverRecipeFile);
+        return fm;
+    }
+    @SneakyThrows
+    public static boolean EnsureResourceUpdate(final String resourceName, final File file, final FileConfiguration fileConfig, final JavaPlugin plugin) {
+        if (!file.exists()) {
+            plugin.saveResource(resourceName, false);
+            return false;
+        }
+        final Reader jarConfigReader = new InputStreamReader(plugin.getResource(resourceName));
+        final FileConfiguration jarResourceConfig = YamlConfiguration.loadConfiguration(jarConfigReader);
+        jarConfigReader.close();
+        boolean unsavedChanges = false;
+        for (final String key : jarResourceConfig.getKeys(false)) {
+            if (ServerVersion.newerThan(ServerVersion.V1_8)) {
+                if (!fileConfig.contains(key, false)) {
+                    fileConfig.set(key, jarResourceConfig.get(key));
+                    unsavedChanges = true;
+                }
+            } else {
+                if (!fileConfig.contains(key)) {
+                    fileConfig.set(key, jarResourceConfig.get(key));
+                    unsavedChanges = true;
+                }
+            }
+        }
+        if (unsavedChanges)
+            fileConfig.save(file);
+        return true;
+    }
+    private File ensureCreated(final File file) {
+        if (!file.exists()) {
+            logger.info(file.getName() + " doesn't exist... creating it.");
+            try {
+                file.createNewFile();
+            } catch (final IOException e) {
+                logger.warning("The file " + file.getName() + " couldn't be created!");
+            }
+        }
+        return file;
+    }
+    private File getFile(final String name) {
+        final File file = new File(dataFolder, name);
+        ensureCreated(file);
+        return file;
+    }
+    private FileConfiguration getYamlConfig(final File file) {
+        try {
+            return YamlConfiguration.loadConfiguration(file);
+        } catch (Exception e) {
+            logger.severe("Failed to load YAML file " + file.getName() + ": " + e.getMessage());
+            return new YamlConfiguration();
+        }
+    }
+    public void cacheRecipes() {
+        Debug.Send("The file manager is caching recipes...");
+        EnhancedRecipe keyValue;
+        if (!recipesFile.exists())
+            return;
+        recipesConfig = getYamlConfig(recipesFile);
+        recipes.clear();
+        for (final String key : recipesConfig.getKeys(false)) {
+            Debug.Send("Caching recipe with key " + key);
+            keyValue = (EnhancedRecipe) recipesConfig.get(key);
+            final String validation = keyValue.validate();
+            if (validation != null) {
+                Messenger.Error("Recipe with key " + key + " has issues: " + validation);
+                Messenger.Error("This recipe will not be cached and loaded.");
+                continue;
+            }
+            keyValue.setKey(key);
+            cacheRecipes.add(keyValue);
+        }
+        cacheRecipes.save();
+        this.recipesFile.renameTo(new File(dataFolder, "recipe_copy.yml"));
+    }
+    @SneakyThrows
+    public void cacheItems() {
+        if (!itemsFile.exists())
+            return;
+        if (useJson) {
+            final StringBuilder json = new StringBuilder();
+            final Scanner scanner = new Scanner(itemsFile);
+            while (scanner.hasNextLine())
+                json.append(scanner.nextLine());
+            scanner.close();
+            items.clear();
+            final Type typeToken = new TypeToken<HashMap<String, Map<String, Object>>>() {}.getType();
+            final Gson gson = new Gson();
+            final Map<String, Map<String, Object>> serialized = gson.fromJson(json.toString(), typeToken);
+            if (serialized != null)
+                serialized.keySet().forEach(x -> items.put(x, ItemStack.deserialize(serialized.get(x))));
+        } else {
+            if (itemsConfig == null)
+                itemsConfig = new YamlConfiguration();
+            itemsConfig.load(itemsFile);
+            items.clear();
+            if (itemsConfig != null)
+                for (final String key : itemsConfig.getKeys(false)) {
+                    items.put(key, itemsConfig.getItemStack(key));
+                }
+        }
+        this.itemsFile.renameTo(new File(dataFolder, useJson ? "items-copy.json" : "items-copy.yml"));
+    }
 
 	public Map<String, ItemStack> getItems() {
 		return items;
@@ -263,55 +244,68 @@ public class FileManager {
 		return false;
 	}
 
-	public List<String> readDisabledServerRecipes() {
-		if (serverRecipeConfig == null)
-			serverRecipeConfig = getYamlConfig(serverRecipeFile);
-		return serverRecipeConfig.getStringList("disabled");
-	}
+    public List<String> readDisabledServerRecipes() {
+        if (serverRecipeConfig == null) {
+            serverRecipeConfig = getYamlConfig(serverRecipeFile);
+        }
+        return serverRecipeConfig.getStringList("disabled");
+    }
+    public boolean saveDisabledServerRecipes(final List<String> keys) {
+        serverRecipeConfig = getYamlConfig(serverRecipeFile);
+        if (serverRecipeConfig == null) {
+            logger.severe("server-recipes.yml is null! Save failed.");
+            return false;
+        }
+        serverRecipeConfig.set("disabled", keys);
+        try {
+            serverRecipeConfig.save(serverRecipeFile);
+            return true;
+        } catch (IOException e) {
+            logger.severe("Failed to save server-recipes.yml: " + e.getMessage());
+            return false;
+        }
+    }
 
-	public boolean saveDisabledServerRecipes(final List<String> keys) {
-		serverRecipeConfig.set("disabled", keys);
-		try {
-			serverRecipeConfig.save(serverRecipeFile);
-		} catch (final IOException e) {
-			return false;
-		}
-		return true;
-	}
-
-	public Map<Location, UUID> getContainerOwners() {
-		containerOwnerConfig = getYamlConfig(containerOwnerFile);
-		final Map<Location, UUID> blockOwners = new HashMap<>();
-		for (final String key : containerOwnerConfig.getKeys(false)) {
-			if (key == null) continue;
-			final String[] parsedKey = key.split(",");
-			final World world = Bukkit.getServer().getWorld(UUID.fromString(parsedKey[3]));
-			if (world != null) {
-				final Location loc = new Location(
-						world,
-						Integer.parseInt(parsedKey[0]),
-						Integer.parseInt(parsedKey[1]),
-						Integer.parseInt(parsedKey[2]));
-				blockOwners.put(loc, UUID.fromString(containerOwnerConfig.getString(key)));
-			}
-		}
-		return blockOwners;
-	}
-
-	public boolean saveContainerOwners(final Map<Location, UUID> blockOwners) {
-		containerOwnerConfig.getKeys(false).forEach(x -> containerOwnerConfig.set(x, null));
-		for (final Map.Entry<Location, UUID> blockOwnerSet : blockOwners.entrySet()) {
-			final Location key = blockOwnerSet.getKey();
-			final String keyString = key.getBlockX() + "," + key.getBlockY() + "," + key.getBlockZ() + "," + key.getWorld().getUID();
-			containerOwnerConfig.set(keyString, blockOwnerSet.getValue().toString());
-		}
-		try {
-			containerOwnerConfig.save(containerOwnerFile);
-		} catch (final IOException e) {
-			return false;
-		}
-		return true;
-	}
+    public Map<Location, UUID> getContainerOwners() {
+        containerOwnerConfig = getYamlConfig(containerOwnerFile);
+        final Map<Location, UUID> blockOwners = new HashMap<>();
+        for (final String key : containerOwnerConfig.getKeys(false)) {
+            if (key == null) continue;
+            final String[] parsedKey = key.split(",");
+            final World world = Bukkit.getServer().getWorld(UUID.fromString(parsedKey[3]));
+            if (world != null) {
+                final Location loc = new Location(
+                        world,
+                        Integer.parseInt(parsedKey[0]),
+                        Integer.parseInt(parsedKey[1]),
+                        Integer.parseInt(parsedKey[2]));
+                blockOwners.put(loc, UUID.fromString(containerOwnerConfig.getString(key)));
+            }
+        }
+        return blockOwners;
+    }
+    public boolean saveContainerOwners(final Map<Location, UUID> blockOwners) {
+        containerOwnerConfig = getYamlConfig(containerOwnerFile);
+        if (containerOwnerConfig == null) {
+            logger.severe("container_owners.yml is null! Save failed.");
+            return false;
+        }
+        for (String key : containerOwnerConfig.getKeys(false)) {
+            containerOwnerConfig.set(key, null);
+        }
+        for (final Map.Entry<Location, UUID> entry : blockOwners.entrySet()) {
+            final Location loc = entry.getKey();
+            final String key = loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + "," + loc.getWorld().getUID();
+            containerOwnerConfig.set(key, entry.getValue().toString());
+        }
+        try {
+            containerOwnerConfig.save(containerOwnerFile);
+            return true;
+        } catch (IOException e) {
+            logger.severe("Failed to save container_owners.yml: " + e.getMessage());
+            return false;
+        }
+    }
 
 	public void saveRecipe(final EnhancedRecipe recipe) {
 		Debug.Send("Saving recipe " + recipe.toString() + " with key " + recipe.getKey());
