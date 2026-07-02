66549d3d7fa55a83740b02184f5d56230b021f7e
broken1arrow <broken2arrow1@gmail.com>
2026-07-02 17:49:51 +0200

    Fixing spelling

--- src/main/java/com/dutchjelly/craftenhance/util/PermissionTypes.java
+++ src/main/java/com/dutchjelly/craftenhance/util/PermissionTypes.java
@@ -3,22 +3,26 @@ package com.dutchjelly.craftenhance.util;
 import com.dutchjelly.craftenhance.CraftEnhance;
 
 public enum PermissionTypes {
-    Edit("perms.recipe-editor"),
-    View("perms.recipe-viewer"),
-    View_ALL("perms.view-all"),
-    EditItem("perms.edit-item"),
-    Category_editor("perms.categorys-editor"),;
+	Edit("perms.recipe-editor"),
+	View("perms.recipe-viewer"),
+	View_ALL("perms.view-all"),
+	EditItem("perms.edit-item"),
+	Category_editor("perms.category-editor"),
+	;
 
-    public final String permPath;
+	public final String permPath;
 
-    PermissionTypes(final String permPath){
-        this.permPath = permPath;
-    }
+	PermissionTypes(final String permPath) {
+		this.permPath = permPath;
+	}
 
-    public String getPerm() {
-        String permission = CraftEnhance.self().getConfig().getString(permPath);
-        if (permission == null)
-            permission = "craftenhance.permission.not.set";
-        return permission;
-    }
+	public String getPerm() {
+		String permission = CraftEnhance.self().getConfig().getString(permPath);
+		if (this == Category_editor && permission == null) {
+			permission = CraftEnhance.self().getConfig().getString("perms.categorys-editor");
+		}
+		if (permission == null)
+			permission = "craftenhance.permission.not.set";
+		return permission;
+	}
 }
